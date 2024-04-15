ad052576a8277853afa31a79b6a7a63aca8c363f
tiarawulan <165629899+tiarawulan@users.noreply.github.com>
2024-04-15 20:14:21 +0700

    Add files via upload
    SQL_1

---
+++ sql_1.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee_table (employee, salary) VALUES
+('Alice', 11),
+('Benn', 43),
+('Charles', 33),
+('Dorothy', 55),
+('Emma', 22),
+('Franklin', 33);
+
+SELECT salary
+FROM employee_table
+ORDER BY salary DESC
+LIMIT 1 OFFSET 2;
